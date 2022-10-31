c121aabacace52a09a61364e6fa57ecb476b31ca
Denis <hvorerden@hotmail.com>
2022-10-31 15:59:02 +0100

    lesson 1

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,7 +1,22 @@
+import { getWorkshops } from '../api'
+import {Link } from 'react-router-dom'
+
 export default function Workshops() {
+
+  const workshops = getWorkshops()
+
   return (
-      <div className="container">
-          <h1>Workshop Categories</h1>
-      </div>
+    <div className="container">
+      <h1>Workshop Categories</h1>
+   
+        {
+          workshops.map(workshop => 
+            <p>{workshop.name}</p>
+                
+                
+                )
+        }
+     
+    </div>
   );
 }
